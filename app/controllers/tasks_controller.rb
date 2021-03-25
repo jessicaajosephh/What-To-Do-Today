f7a7b6fad64790259112fa329b5fdbb1cd33c5fa
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-25 18:06:32 -0400

    added basic code to the get methods in the tasks controller file to see if i can get the app to function properly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
 
     get '/tasks' do
-
+        erb :'/tasks/show'
     end
 
     get '/tasks/new' do
-
+        erb :'/tasks/new'
     end
 
     post '/tasks/new' do
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
     end
 
     get '/tasks/new/:id' do 
-
+        erb :'/tasks/add_to_list'
     end
 
     post '/tasks/new/:id' do
@@ -21,7 +21,7 @@ class TasksController < ApplicationController
     end
 
     get 'tasks/:id/edit' do
-
+        erb :'/tasks/edit'
     end
 
     patch '/tasks/:id' do
@@ -29,7 +29,7 @@ class TasksController < ApplicationController
     end
 
     get '/tasks/:id/delete' do
-
+        erb :'/tasks/delete'
     end
 
     delete '/tasks/:id' do
