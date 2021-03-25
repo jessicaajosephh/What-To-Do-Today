cd563f6e7dde32a979f1c8ed5ab6a9110ea3bc1f
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-25 13:43:36 -0400

    created all the get, post, patch and delete methods for the lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,27 @@
 class ListsController < ApplicationController
 
+    get '/lists/new' do 
+
+    end
+
+    post '/lists/new' do
+
+    end
+
+    get '/lists/:id/edit' do
+
+    end
+
+    patch '/lists/:id' do
+
+    end
+
+    get '/lists/:id/delete' do
+
+    end
+
+    delete '/lists/:id' do
+
+    end
+
 end
