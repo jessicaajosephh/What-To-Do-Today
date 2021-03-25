e9e2f14efb12bcb89aa3b937482456fb66a99b67
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-25 13:03:30 -0400

    updated all my code to factor in the new table lists

--- db/migrate/20210324154150_create_tasks.rb
+++ db/migrate/20210324154150_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.string :description
       t.integer :user_id
+      t.integer :list_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_154150) do
+ActiveRecord::Schema.define(version: 2021_03_25_165404) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.string "description"
     t.integer "user_id"
+    t.integer "list_id"
   end
 
   create_table "users", force: :cascade do |t|
