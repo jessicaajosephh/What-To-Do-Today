b88f17e4c3abea0faf30f3763cb471002c308c07
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-25 13:07:14 -0400

    added to the class User to validate the name and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
     has_many :lists
     has_many :tasks, through: :lists
     has_secure_password
+
+    validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
 end
