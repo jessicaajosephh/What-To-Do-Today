18489f64a784a493229f48082396f84ec47694f7
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-25 13:39:43 -0400

    created all my get, post, patch and delete methods for the tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,40 @@
 class TasksController < ApplicationController
 
+    get '/tasks' do
+
+    end
+
+    get '/tasks/new' do
+
+    end
+
+    post '/tasks/new' do
+
+    end
+
+    get '/tasks/new/:id' do 
+
+    end
+
+    post '/tasks/new/:id' do
+
+    end
+
+    get 'tasks/:id/edit' do
+
+    end
+
+    patch '/tasks/:id' do
+
+    end
+
+    get '/tasks/:id/delete' do
+
+    end
+
+    delete '/tasks/:id' do
+
+    end
+
 
 end
