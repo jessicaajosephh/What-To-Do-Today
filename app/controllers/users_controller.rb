31a3a2e97a2531937114de14d6e7f86bd5cc24da
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-25 21:30:12 -0400

    added in code to user controller to make sure app is functioning properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,48 @@
 class UsersController < ApplicationController
 
     get '/users/signup' do 
-        erb :'users/signup'
+        if logged_in?
+            redirect to '/tasks'
+        else
+            erb :'users/signup'
+        end
     end
 
     post '/users/signup' do
-
+        if params[:username] == "" || params[:password] == ""
+            redirect to '/users/signup'
+        else
+            @user = User.create(:username => params[:username], :password => params[:password])
+            session[:user_id] = @user.id
+            redirect to '/tasks'
+        end
     end
 
     get '/users/login' do
-        erb :'users/login'
+        if logged_in?
+            redirect to '/tasks'
+        else
+            erb :'users/login'
+        end
     end
 
     post '/users/login' do
-
+        @user = User.find_by(:username => params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to '/tasks'
+        else
+            redirect to '/users/login'
+        end
     end
 
     get '/users/logout' do
-
+        if session[:user_id] != nil 
+            session.clear
+            redirect to '/users/login'
+        else
+            redirect to '/'
+        end
     end
 
 
